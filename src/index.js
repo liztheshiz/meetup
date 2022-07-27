0397347d1d9f196a1f6aacbe92c33cef5399027a
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-07-27 17:14:58 -0400

    fixed service worker typo and activated register

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 import './index.css';
 
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './service-worker';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
@@ -23,7 +23,7 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 //serviceWorkerRegistration.unregister();
-serviceWorkerRegistration.unregister();
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
