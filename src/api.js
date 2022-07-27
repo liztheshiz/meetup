aa87368a4203d4f5bebb0a859a31928fe480b3a4
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-07-27 13:56:16 -0400

    made last events available offline

--- src/api.js
+++ src/api.js
@@ -47,6 +47,13 @@ export const getEvents = async () => {
         return mockData;
     }
 
+    // If offline, load data from localStorage
+    if (!navigator.onLine) {
+        const data = localStorage.getItem("lastEvents");
+        NProgress.done();
+        return data ? JSON.parse(events).events : [];;
+    }
+
     const token = await getAccessToken();
 
     if (token) {
