0d317fedf4f01c123e4752f6e2c9b5fc2c639c87
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-07-27 17:13:50 -0400

    unregistered service worker for bug fix

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 //serviceWorkerRegistration.unregister();
-serviceWorker.register();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
