b83b06d896f7515c0295422fb213bb91d4552bbe
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-08-01 11:03:48 -0400

    WORKS OFFLINE

--- src/App.js
+++ src/App.js
@@ -68,8 +68,12 @@ class App extends React.Component {
     async componentDidMount() {
         this.mounted = true;
         const accessToken = localStorage.getItem('access_token');
-        const isTokenValid = (await checkToken(accessToken)).error ? false :
-            true;
+        let isTokenValid;
+        if (accessToken && !navigator.onLine) {
+            isTokenValid = true;
+        } else {
+            isTokenValid = (await checkToken(accessToken)).error ? false : true;
+        }
         const searchParams = new URLSearchParams(window.location.search);
         const code = searchParams.get("code");
         this.setState({ showWelcomeScreen: !(code || isTokenValid) });
--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import nProgress from 'nprogress';
+import NProgress from 'nprogress';
 
 import { mockData } from './mock-data';
 
@@ -8,74 +8,35 @@ This function takes an events array, then uses map to create a new array with on
 It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
 The Set will remove all duplicates from the array.
 */
-export const extractLocations = (events) => {
-    var extractLocations = events.map((event) => event.location);
-    var locations = [...new Set(extractLocations)];
-    return locations;
-};
-
 export const checkToken = async (accessToken) => {
-    const result = await fetch(
-        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-    )
-        .then((res) => res.json())
-        .catch((error) => error.json());
-
-    return result;
-};
-
-// Removes code from url (once we've gotten what we need from it)
-const removeQuery = () => {
-    if (window.history.pushState && window.location.pathname) {
-        var newurl =
-            window.location.protocol +
-            '//' +
-            window.location.host +
-            window.location.pathname;
-        window.history.pushState('', '', newurl);
-    } else {
-        newurl = window.location.protocol + '//' + window.location.host;
-        window.history.pushState('', '', newurl);
-    }
-};
-
-export const getEvents = async () => {
-    nProgress.start();
-
-    if (window.location.href.startsWith('http://localhost')) {
-        nProgress.done();
-        return mockData;
-    }
-
-    const token = await getAccessToken();
-
-    if (token) {
-        removeQuery();
-        const url = 'https://7dz6lq0do3.execute-api.us-east-1.amazonaws.com/dev/api/get-events/' + token;
-        const result = await axios.get(url);
-        if (result.data) {
-            var locations = extractLocations(result.data.events);
-            localStorage.setItem('lastEvents', JSON.stringify(result.data));
-            localStorage.setItem('locations', JSON.stringify(locations));
-        }
-        nProgress.done();
-        return result.data.events;
+    try {
+        const result = await fetch(
+            `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+        );
+        return await result.json();
+    } catch (error) {
+        error.json();
     }
 };
 
 const getToken = async (code) => {
-    const encodeCode = encodeURIComponent(code);
-    const { access_token } = await fetch(
-        'https://7dz6lq0do3.execute-api.us-east-1.amazonaws.com/dev/api/token/' + encodeCode
-    )
-        .then((res) => {
-            return res.json();
-        })
-        .catch((error) => error);
+    try {
+        const encodeCode = encodeURIComponent(code);
+        const response = await fetch(
+            'https://7dz6lq0do3.execute-api.us-east-1.amazonaws.com/dev/api/token' +
+            '/' +
+            encodeCode
+        );
 
-    access_token && localStorage.setItem("access_token", access_token);
-
-    return access_token;
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const { access_token } = await response.json();
+        access_token && localStorage.setItem('access_token', access_token);
+        return access_token;
+    } catch (error) {
+        error.json();
+    }
 };
 
 export const getAccessToken = async () => {
@@ -96,4 +57,56 @@ export const getAccessToken = async () => {
         return code && getToken(code);
     }
     return accessToken;
+};
+
+export const extractLocations = (events) => {
+    var extractLocations = events.map((event) => event.location);
+    var locations = [...new Set(extractLocations)];
+    return locations;
+};
+
+const removeQuery = () => {
+    if (window.history.pushState && window.location.pathname) {
+        var newurl =
+            window.location.protocol +
+            '//' +
+            window.location.host +
+            window.location.pathname;
+        window.history.pushState('', '', newurl);
+    } else {
+        newurl = window.location.protocol + '//' + window.location.host;
+        window.history.pushState('', '', newurl);
+    }
+};
+
+export const getEvents = async () => {
+    NProgress.start();
+
+    if (window.location.href.startsWith('http://localhost')) {
+        return mockData;
+    }
+
+    if (!navigator.onLine) {
+        const data = localStorage.getItem('lastEvents');
+        NProgress.done();
+        return data ? JSON.parse(data).events : [];
+    }
+
+    const token = await getAccessToken();
+
+    if (token) {
+        removeQuery();
+        const url =
+            'https://7dz6lq0do3.execute-api.us-east-1.amazonaws.com/dev/api/get-events' +
+            '/' +
+            token;
+        const result = await axios.get(url);
+        if (result.data) {
+            var locations = extractLocations(result.data.events);
+            localStorage.setItem('lastEvents', JSON.stringify(result.data));
+            localStorage.setItem('locations', JSON.stringify(locations));
+        }
+        NProgress.done();
+        return result.data.events;
+    }
 };
